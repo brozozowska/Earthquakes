1a22970d1a7bfb3f18e891e163343b9d30119eb3
brozozowska <s_rozov@me.com>
2024-06-09 20:34:24 +0300

    Chapter 1. Building a network test client - complete.

---
+++ Earthquakes/Models/QuakesProvider.swift
@@ -0,0 +1,30 @@
+//
+//  QuakesProvider.swift
+//  EarthquakesTests
+//
+//  Created by Сергей Розов on 09.06.2024.
+//  Copyright © 2024 Apple. All rights reserved.
+//
+
+import Foundation
+
+@MainActor
+class QuakesProvider: ObservableObject {
+    
+    @Published var quakes: [Quake] = []
+    
+    let client: QuakeClient
+    
+    func fetchQuakes() async throws {
+        let latestQuakes = try await client.quakes
+        self.quakes = latestQuakes
+    }
+    
+    func deleteQuakes(atOffsets offsets: IndexSet) {
+        quakes.remove(atOffsets: offsets)
+    }
+    
+    init(client: QuakeClient = QuakeClient()) {
+        self.client = client
+    }
+}
